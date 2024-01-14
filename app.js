75e94a3b7556a2eed12624cb775d80290581003c
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-01-14 14:00:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,15 @@
     const cesiumAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNWJlYzdlYi03OWE2LTQ4NDktYjU1MS0wMjg4MWIzMDI0YmEiLCJpZCI6MTczNDE4LCJpYXQiOjE3MDE2MjM1OTZ9.UMTbFZ4HZz2IJbfsVFFsob7GgDE1haShx5DWUdhrkr4";
     Cesium.Ion.defaultAccessToken = cesiumAccessToken;
 
-     // Initialize the Cesium Viewer
-const viewer = new Cesium.Viewer('cesiumContainer');
+  // Initialize the Cesium Viewer
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+        imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+            url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+        }),
+        baseLayerPicker: false,
+        geocoder: false,
+        sceneModePicker: false
     });
-
 
     viewer.scene.camera.setView({
         destination: Cesium.Cartesian3.fromDegrees(-118.220071, 33.766145, 10000.0),
