5736ede55f6c815978bf7f1e5b70dd37380d8b72
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-01-23 13:35:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -41,16 +41,15 @@
     const scenes = [
         {
             title: "1: The Green Port",
-            content: "<p></p><p>The Port of Long Beach (POLB), proudly known as The Green Port, is a leader in green port operations. It plays a vital role in the U.S. and global economy, with its strategic location in Long Beach, California. The Port of Long Beach remains dedicated to pioneering green initiatives, integrating economic activity with environmental stewardship and community well-being. Among its awards and recognitions for its sustainable practices are the Level 4 Green Marine Environmental Certification, 2020 California Governor's Environmental and Economic Leadership Award, and the 2019 AAPA Environmental Excellence Award.</p>" +  
-
+            content: "<p></p>" + "<p>The Port of Long Beach (POLB), proudly known as The Green Port, is a leader in green port operations. It plays a vital role in the U.S. and global economy, with its strategic location in Long Beach, California. The Port of Long Beach remains dedicated to pioneering green initiatives, integrating economic activity with environmental stewardship and community well-being. Among its awards and recognitions for its sustainable practices are the Level 4 Green Marine Environmental Certification, 2020 California Governor's Environmental and Economic Leadership Award, and the 2019 AAPA Environmental Excellence Award.</p>" +  
 "<p>It's sustainable practices and goals include:</p>" +
-
+"<ul>" +
 "<p><li>Clean Air Action Plan (CAAP): Targets 100% zero-emission trucks by 2035 and cargo equipment by 2030.</li><p>" +
 "<p><li>Green Port Policy (2005): Addresses wildlife, air, water, soils, community engagement, and sustainability.</li></p>" +
 "<p><li>Zero-Emissions Technologies: Focused on electric vehicles and infrastructure for a zero-emissions future.</li></p>" + 
 "<p><li>Sustainable Terminal Design: Emphasizes energy-efficient, water-conserving, and waste-reducing practices.</li></p>" + 
-"<p><li>Community Engagement: Prioritizes environmental justice and public health in local communities.</li></p>",  
-
+"<p><li>Community Engagement: Prioritizes environmental justice and public health in local communities.</li></p>" + 
+                "</ul>",  
         destination: Cesium.Cartesian3.fromDegrees(-118.2765, 33.7470, 2500),
         orientation: {
           heading : Cesium.Math.toRadians(90.0), // East, in radians
