234abf104f24a5e30c2d02b7f7e4c8993fbf92ae
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-01-23 17:16:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -50,9 +50,9 @@
 "<p><li>Sustainable Terminal Design: Emphasizes energy-efficient, water-conserving, and waste-reducing practices.</li></p>" + 
 "<p><li>Community Engagement: Prioritizes environmental justice and public health in local communities.</li></p>" + 
                 "</ul>",  
-        destination: Cesium.Cartesian3.fromDegrees(-118.12, 33.43, 15000),
+        destination: Cesium.Cartesian3.fromDegrees(-118.12, 33.41, 12000),
         orientation: {
-          heading : Cesium.Math.toRadians(20.0), // 
+          heading : Cesium.Math.toRadians(45.0), // 
           pitch : Cesium.Math.toRadians(-45.0), // Looking down, in radians
           roll : 0.0 // No roll
     }
