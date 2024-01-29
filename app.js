66a96ae413028a167a3512f07252608dec27cb76
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-01-29 00:46:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@
         Cesium.Cartesian3.fromDegrees(-118.2360, 33.7500, 12000), // Middle Harbor
         Cesium.Cartesian3.fromDegrees(-118.2250, 33.7390, 8000), // Long Beach Container Terminal
         Cesium.Cartesian3.fromDegrees(-118.1675, 33.7705, 6000), // Downtown 
-        Cesium.Cartesian3.fromDegrees(-118.1560, 33.7950, 1500)  // Bluff Park (Residential Area)
+        Cesium.Cartesian3.fromDegrees(-118.1570, 33.7990, 1500)  // Bluff Park (Residential Area)
         // Add more locations as needed
     ];
 
