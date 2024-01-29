589cd66e00f0ece1f8d2aad268a1ff9a136de4ca
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-01-29 14:11:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@
  const locations = [
         Cesium.Cartesian3.fromDegrees(-118.2485, 33.7489, 30000), // Vincent Thomas Bridge
         Cesium.Cartesian3.fromDegrees(-118.2340, 33.7500, 12000), // Middle Harbor
-        Cesium.Cartesian3.fromDegrees(-118.2230, 33.7390, 8000), // Long Beach Container Terminal
+        Cesium.Cartesian3.fromDegrees(-118.2230, 33.7400, 8000), // Long Beach Container Terminal
         Cesium.Cartesian3.fromDegrees(-118.2140, 33.7320, 4000),  // Bluff Park (Residential Area)
         Cesium.Cartesian3.fromDegrees(-118.1685, 33.7705, 2500),  // Downtown 
         Cesium.Cartesian3.fromDegrees(-118.1800, 33.7905, 1500)  // Downtown 
