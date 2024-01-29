29c36076a4e4ba86f8f700da93bf05b11b8553f0
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-01-28 23:33:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+// Section 1: API Keys and Viewer Initialization
+(function() {
+    var currentSceneIndex = 0;
+    var currentSlideIndex = 0;
+    var slides;
+    var viewer; 
+    
+    const apiKey = "AAPK0dc01961f9f84d51999214b2d7ca7ff6x6uGDqE0RJUvSzovTBuHrsjDNrutFT4xmERUGjjwJyxRD20vlXQvtIEPtAzSAOb7";
+    Cesium.ArcGisMapService.defaultAccessToken = apiKey;
 
+    const cesiumAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNWJlYzdlYi03OWE2LTQ4NDktYjU1MS0wMjg4MWIzMDI0YmEiLCJpZCI6MTczNDE4LCJpYXQiOjE3MDE2MjM1OTZ9.UMTbFZ4HZz2IJbfsVFFsob7GgDE1haShx5DWUdhrkr4";
+    Cesium.Ion.defaultAccessToken = cesiumAccessToken;
+
+   // Initialize the Cesium Viewer
+    viewer = new Cesium.Viewer('cesiumContainer', {
+    animation: false, // Don't show the animation widget
+    baseLayerPicker: false, // Don't show the base layer picker
+    fullscreenButton: false, // Don't show the fullscreen button
+    vrButton: false, // Don't show the VR button
+    geocoder: false, // Don't show the geocoder widget
+    homeButton: false, // Don't show the home button
+    infoBox: true, // Don't show the info box
+    sceneModePicker: false, // Don't show the scene mode picker
+    selectionIndicator: false, // Don't show the selection indicator
+    timeline: false, // Don't show the timeline
+    navigationHelpButton: false, // Don't show the navigation help button
+});
+
+ })();
