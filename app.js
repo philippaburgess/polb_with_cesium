37cb981517d6085fc469a901778bceacb3cb1625
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-02-01 15:38:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -365,6 +365,7 @@ if (currentSceneIndex === 12) { // Scene index starts at 0, so index 12 is Scene
             orientation: scene.orientation,
             duration: 2  // Duration of the camera flight in seconds
         });
+}
 
 // Section 4 
 
@@ -516,4 +517,5 @@ var container = document.getElementById('cesiumContainer');
             toggleAirQualityLayer(); // No need to pass currentSceneIndex as it's a global variable
         });
 }
+})
 })();
