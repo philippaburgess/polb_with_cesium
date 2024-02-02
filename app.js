da32ebb836059ea173d92af2fa1b24af13f4957b
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-02-01 20:16:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -514,7 +514,6 @@ window.closeInstructions = function() {
 
         button.addEventListener('click', function() {
             toggleAirQualityLayer(); // This will call your toggle function
-       });
-}
-})
-})();
+        });
+    }
+})(); 
