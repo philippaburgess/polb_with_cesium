f8025fd11b77f51cf15e4f462562f26bd94cc3a7
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-02-03 21:11:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -366,13 +366,15 @@ if (currentSceneIndex === 12) { // Scene index starts at 0, so index 12 is Scene
         }
    }); 
 
+ if (scene.destination && scene.orientation) {
         viewer.camera.flyTo({
             destination: scene.destination,
             orientation: scene.orientation,
             duration: 2  // Duration of the camera flight in seconds
         });
+    }
 }
-}
+
 // Section 4 
 
     function displayInfoBox(pickedFeature) {        
