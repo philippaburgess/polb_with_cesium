c7d61434bdfc2a0ad4af46d91c9842076f71556d
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-02-08 15:58:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -447,6 +447,7 @@ if (currentSceneIndex === 12) { // Scene index starts at 0, so index 12 is Scene
                 longBeachDataLayer = null;
             }
         }
+    }
 
 //        viewer.camera.flyTo({
 //            destination: scene.destination,
