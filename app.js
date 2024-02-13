4d7de1f2d9107960c989a5a17a8fac8793e5ecd5
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-02-12 20:13:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -369,7 +369,7 @@ function flyToScene(scene) {
             complete: function() {
                 // After arriving at the above water location, fly to underwater
                 viewer.camera.flyTo({
-                    destination: Cesium.Cartesian3.fromDegrees(-118.2266, 33.7450, -20), // Replace with underwater coordinates
+                    destination: Cesium.Cartesian3.fromDegrees(-118.2266, 33.7440, -20), // Replace with underwater coordinates
                     orientation: {
                         heading: Cesium.Math.toRadians(0), // Replace with desired heading
                         pitch: Cesium.Math.toRadians(-10.0), // Replace with desired pitch
