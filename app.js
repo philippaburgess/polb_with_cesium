ff044d133aaa55ca3d4b5120e755f269c599fdfb
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-02-14 10:25:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -288,19 +288,13 @@ function setSceneContent(scene) {
       document.getElementById('scene-container').style.display = 'block';
  }
 
-    var scene = scenes[sceneIndex];
-    setSceneContent(scene);
-    manageHeatmapVisibility(sceneIndex);
-    checkSceneForGeoJsonLayers(sceneIndex);
-}
-
-// function initHeatmapLayerProvider() {
-//   if (!heatmapImageryProvider) {
-//       heatmapImageryProvider = new Cesium.UrlTemplateImageryProvider({
-//            url: `https://airquality.googleapis.com/v1/mapTypes/${airQualityMapType}/heatmapTiles/{z}/{x}/{y}?key=${airQualityApiKey}`
-//        });
-//    }
-// }
+ function initHeatmapLayerProvider() {
+   if (!heatmapImageryProvider) {
+       heatmapImageryProvider = new Cesium.UrlTemplateImageryProvider({
+            url: `https://airquality.googleapis.com/v1/mapTypes/${airQualityMapType}/heatmapTiles/{z}/{x}/{y}?key=${airQualityApiKey}`
+        });
+    }
+ }
 
 function updateToggleAndHeatmapVisibility(sceneIndex) {
     // The toggle button and heatmap functionality start from Scene 8
